0742a2a95dc0dc7cc4bb9dd565e080744965d940
step24 <32043447+step24@users.noreply.github.com>
2020-09-27 19:13:59 +0300

    Add files via upload

---
+++ DWH startup investments/insert data scripts/buffer/incremental_load_202006142156.sql
@@ -0,0 +1,6 @@
+INSERT INTO staging_area.incremental_load (table_name,last_load_date) VALUES 
+('objects','2020-06-14 19:21:17.119')
+,('investments','2020-06-14 19:21:17.598')
+,('funding_round','2020-06-14 19:21:18.211')
+,('ipo','2020-06-14 19:21:18.381')
+;
