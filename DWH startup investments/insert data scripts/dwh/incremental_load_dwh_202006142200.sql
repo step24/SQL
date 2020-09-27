71f2808c1cb6a7a077e347730102da2bee1866fa
step24 <32043447+step24@users.noreply.github.com>
2020-09-27 19:16:05 +0300

    Add files via upload

---
+++ DWH startup investments/insert data scripts/dwh/incremental_load_dwh_202006142200.sql
@@ -0,0 +1,7 @@
+INSERT INTO dwh.incremental_load_dwh (table_name,last_load_date) VALUES 
+('round_sats','2020-06-14 19:23:42.123')
+,('object_sats','2020-06-14 19:23:48.379')
+,('ipo_sats','2020-06-14 19:23:50.163')
+,('hub_date_by_ipo','1950-06-12 17:02:23.000')
+,('hub_date_by_investment','1950-06-12 17:02:23.000')
+;
